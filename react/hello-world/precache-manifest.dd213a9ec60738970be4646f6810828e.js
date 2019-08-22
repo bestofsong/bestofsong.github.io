99b3f6685fdfe79e12b0449020b6f6efeda715c2
wansong <betterofsong@gmail.com>
2019-08-22 23:57:05 +0800

    add react-demo

---
+++ react/hello-world/precache-manifest.dd213a9ec60738970be4646f6810828e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dda9b68f8d5d005e2a6c8185ee62d5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f7e9ebdbe14263755d4",
+    "url": "./static/css/main.3ad1fb8a.chunk.css"
+  },
+  {
+    "revision": "0bb6d7115e1caa5d31a5",
+    "url": "./static/js/2.d80b0ff6.chunk.js"
+  },
+  {
+    "revision": "4f7e9ebdbe14263755d4",
+    "url": "./static/js/main.96625610.chunk.js"
+  },
+  {
+    "revision": "2210846e045ee13d5e52",
+    "url": "./static/js/runtime~main.d279d1ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
